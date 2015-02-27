50f900c4bd75ba46e18b3889bdab9cae46a97bc9
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-27 09:40:27 -0800

    Add app.js file

---
+++ app.js
@@ -0,0 +1,20 @@
+var newsPaper = angular.module('newsPaper', ['ui.router']);
+
+newsPaper.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: '',
+    templateUrl: 'partials/home.html'
+  });
+
+  $stateProvider.state('beats', {
+    url: '/beats',
+    templateUrl: 'partials/beats.html',
+    controller: 'BeatsCtrl'
+  });
+
+  $stateProvider.state('beats.reporters', {
+    url: '/:beatId',
+    templateUrl: 'partials/beats.reporters.html',
+    controller: 'ReportersCtrl'
+  });
+});
