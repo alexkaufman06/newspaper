8f86b2fc765f4faa4266f40a210b01f73b7dc85a
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-27 10:43:03 -0800

    Fix Reporters Controller Error

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,5 +1,5 @@
 newsPaper.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
-  $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.courseId)
+  $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId)
   $scope.addReporter = function() {
     $scope.beat.reporters.push({ name: $scope.reporterName, years: $scope.reporterYears, bday: $scope.reporterBday });
     $scope.reporterName = null;
