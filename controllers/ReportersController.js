28d5e158ec84a41b6995b66af2857e40f7d940c8
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-27 10:00:04 -0800

    Add Reporters Controller

---
+++ controllers/ReportersController.js
@@ -0,0 +1,9 @@
+newsPaper.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
+  $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.courseId)
+  $scope.addReporter = function() {
+    $scope.beat.reporters.push({ name: $scope.reporterName, years: $scope.reporterYears, bday: $scope.reporterBday });
+    $scope.reporterName = null;
+    $scope.reporterYears = null;
+    $scope.reporterBday = null;
+  }
+});
